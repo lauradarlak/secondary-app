4da4ca0451e783d52cc51a1dc781f415fac5a711
lauradarlak <lauradarlak@gmail.com>
2018-12-21 09:40:39 -0500

    Add Devise to user model and Devise views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_and_belongs_to_many :schools
   has_many :essays, through: :schools
   has_many :categories, through: :essays
