b52f351b6cd0f153332e000d7f364a9da02ea38d
lauradarlak <lauradarlak@gmail.com>
2018-12-22 12:00:05 -0500

    add Devise to Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_and_belongs_to_many :schools
   has_many :essays, through: :schools
   has_many :categories, through: :essays
-  has_secure_password
-
+  
   #scope :advisor, -> { where(type: 'Advisor')}
   #scope :student, -> { where(type: 'Student')}
 end
--- db/migrate/20181220213511_create_users.rb
+++ db/migrate/20181220213511_create_users.rb
@@ -4,11 +4,9 @@ class CreateUsers < ActiveRecord::Migration[5.2]
       t.string :first_name
       t.string :last_name
       t.string :email
-      t.string :password_digest
       t.string :type
 
       t.timestamps
     end
-    #add_index :users, :type
   end
 end
